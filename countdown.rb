128440d63296a1f3070804d8d945e8b6932eadc5
Marvin Reyes <reyemarv@gmail.com>
2020-11-09 02:05:51 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,20 @@
 #write your code here
 
-def countdown
+def countdown(x)
+  until x == 0 
+    puts "#{x} SECOND(S)!"
+    x -= 1
+  end
+   "HAPPY NEW YEAR!"
 end
+
+
+def countdown_with_sleep(x)
+    until x == 0 
+    sleep(1)
+    puts "#{x} SECOND(S)!"
+    x -= 1
+  end
+   "HAPPY NEW YEAR!"
+end 
+  
